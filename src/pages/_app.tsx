2c4e7bf3d4ab20f310a8f1680e4cca59d32d1214
VitorFirmino <ursinho@DESKTOP-J1KHNAO.localdomain>
2021-03-01 23:05:27 -0300

    :package: feat: pages added

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/global.css'
+
+import { ThemeProvider } from '../contexts/ThemeContext';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider>
+        <Component {...pageProps} />
+    </ThemeProvider>
+    )
+    
+}
+
+export default MyApp
